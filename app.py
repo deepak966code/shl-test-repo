9709901d6ffbf0399588ab82c284926d36bddd20
deepak966code <143508591+deepak966code@users.noreply.github.com>
2025-04-10 12:57:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,5 +151,5 @@ def api_recommend():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
