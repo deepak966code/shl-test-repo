1e41d4491818efad69c1f68b6c5b7f3032a3870e
deepak966code <143508591+deepak966code@users.noreply.github.com>
2025-04-10 12:44:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,6 +150,6 @@ def api_recommend():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
